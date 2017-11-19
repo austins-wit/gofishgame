28a69a7feec6cc364d656bb5cbdf6fa2a8f88081
leslievongphakdy <vongphakdyl@wit.edu>
2017-11-19 15:52:55 -0600

    Add files via upload
    I put this in a different folder because I don't know how to git correctly. also it has another message on that one lol

--- src/edu/wit/dcsn/comp2000/listapp/Deck.java
+++ src/edu/wit/dcsn/comp2000/listapp/Deck.java
@@ -1,40 +1,60 @@
 package edu.wit.dcsn.comp2000.listapp;
 
-import java.util.List;
+import java.util.LinkedList;
 
 /**
  * A class representing a deck of cards.
  * 
- * @author NO AUTHOR
+ * @author Leslie Vongphakdy
  *
  */
 public class Deck extends Pile {
 
+	private static LinkedList<Card> deck;
+	private static int deckSize = 52;
+	int x;
 	/**
 	 * Create a Deck
+	 * @param x 
 	 */
 	public Deck() {
-		// TODO Implement
+		Pile deck = new Pile();
+		for (x = 0; x < deckSize; x++) {
+			deck.addCard(draw());
+		}
 	}
-	
+
+
 	/**
-	 * Deals hands to each player.
-	 * @param players the list of players
-	 * @param handSize the size of the hands to deal
+	 * deals a hand of 7 cards to players
+	 * @param players
+	 * @param handSize
+	 * @return
 	 */
-	public void deal(List<Player> players, int handSize) {
-		// TODO Implement
+	public int deal(Player players, int handSize ) {
+		handSize = 7;
+		Hand hand = new Hand();
+		for(int x = 0; x < handSize; x++) {
+			hand.addCard(draw());
+		}
+		return 0;
 	}
-	
+
 	/**
 	 * Draws a card from the top of the deck
 	 * @return the card at the top of the deck
 	 */
-	public Card draw() {
+	public static Card draw() {
 		// TODO Implement
-		return null;
+		if(deckSize > 0) {
+			return deck.remove(0);
+		} 
+		else
+		{
+			return null;
+		}
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * @see edu.wit.dcsn.comp2000.listapp.Pile#toString()
@@ -42,7 +62,7 @@ public class Deck extends Pile {
 	@Override
 	public String toString() {
 		// TODO Implement
-		return null;
+		return deck.toString();
 	}
 	
 	/**
@@ -51,5 +71,13 @@ public class Deck extends Pile {
 	 */
 	public static void main(String[] args) {
 		// TODO Implement
+		Deck testDeck = new Deck();
+		System.out.println("Adding cards to new deck...");
+		System.out.println("Deck has: " + testDeck.toString());
+		Player[] players = new Player[2];
+		System.out.println("Creating new hand...");
+		System.out.println("Hand has: " + testDeck.deal(players[2], deckSize));
+		System.out.println("Please take another card.");
+		System.out.println("Card drawn is " + Deck.draw());
 	}
 }
